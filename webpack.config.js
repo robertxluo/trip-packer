c12f75aca878e8e22c37d61e5ff2a6491223c973
Robert Luo <robertxluo@gmail.com>
2020-05-28 21:08:58 -0500

    Complete initial setup with webpack and react

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: process.env.NODE_ENV,
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
