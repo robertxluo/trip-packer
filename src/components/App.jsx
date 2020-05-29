0799fffa0e7652fa7882f03cb471c1813b455d79
Robert Luo <robertxluo@gmail.com>
2020-05-28 22:17:02 -0500

    Header wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import Header from './Header.jsx';
 
 const App = () => {
   return (
-    <div className="container mx-auto">
+    <div className="">
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div class="text-gray-700 body-font">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <span class="ml-3 text-xl">Trip Packer</span>
-        </a>
-        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center"></nav>
-        <button class="inline-flex items-center bg-green-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">
-          Button
-        </button>
-      </div>
+    <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center border-b-2">
+      <a className="flex title-font font-medium items-center text-green-400 mb-4 md:mb-0">
+        <span className="ml-3 text-3xl">trip packer</span>
+      </a>
+      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center"></nav>
+      <button className="inline-flex items-center bg-green-200 border-0 py-2 px-4 focus:outline-none hover:bg-green-300 rounded text-base mt-4 md:mt-0">
+        Button
+      </button>
     </div>
   );
 };
