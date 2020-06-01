f007d6daa6206216f07c2f51c3cfee831045b823
Robert Luo <robertxluo@gmail.com>
2020-06-01 09:30:13 -0500

    Add edit pencil icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,7 +11,12 @@ const Card = ({ items, title }) => {
         <Title text={title} />
         <ul>
           {items.map((item) => (
-            <li>{item.content}</li>
+            <li className="flex items-center" key={item.id}>
+              {item.content}
+              <svg className="ml-2 w-3 h-3 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z" />
+              </svg>{' '}
+            </li>
           ))}
         </ul>
       </div>
