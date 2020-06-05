85e3a52c7d27690fb99347d5969cbdfd99ecfd34
Robert Luo <robertxluo@gmail.com>
2020-06-05 01:57:39 -0500

    Add link to main

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from '@reach/router';
 
 import travel from './../assets/images/travel_mode.svg';
 
@@ -12,9 +13,11 @@ const Home = () => {
             Going on a business trip in Los Angeles, visiting the temples in Kyoto, or going on a road trip-no matter where you want to go. We have
             the perfect trip packer for you.
           </p>
-          <button className="px-6 py-4 mx-8 my-4 mb-4 text-base font-semibold text-gray-100 bg-green-400 border-0 rounded-lg center focus:outline-none hover:bg-green-500">
-            Get Started
-          </button>
+          <Link to="main">
+            <button className="px-6 py-4 mx-8 my-4 mb-4 text-base font-semibold text-gray-100 bg-green-400 border-0 rounded-lg center focus:outline-none hover:bg-green-500">
+              Get Started
+            </button>
+          </Link>
         </div>
         <img className="hidden object-cover w-auto h-auto mt-8 md:inline-block" src={travel} alt="Person dragging Suitcase" />
       </div>
