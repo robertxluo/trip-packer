df2aac96083162cd572a7d0a4e01bd74fe980781
Robert Luo <robertxluo@gmail.com>
2020-06-06 14:10:43 -0500

    Change NavBar button text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from '@reach/router';
 
 const Navbar = () => {
+  const text = localStorage.getItem('destination') ? 'Launch' : 'Sign In';
+
   return (
     <nav className="flex flex-col flex-wrap items-center mx-auto border-b-2 md:p-5 sm:p-0 md:flex-row">
       <Link to="/" className="flex items-center mb-4 font-medium text-green-400 cursor-pointer title-font md:mb-0">
@@ -10,7 +12,7 @@ const Navbar = () => {
       <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto"></nav>
       <Link to="main">
         <button className="inline-flex items-center px-5 py-3 mt-0 mb-4 text-sm font-semibold text-gray-100 bg-green-400 border-0 rounded-full shadow-lg focus:outline-none focus:shadow-outline hover:bg-green-300 active:bg-green-500 md:my-0">
-          Sign In
+          {text}
         </button>
       </Link>
     </nav>
